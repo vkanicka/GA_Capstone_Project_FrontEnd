82c06a2a50f6d67196cd0ebd0bc759b1810ca1e6
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-05-28 15:54:00 -0500

    Button colors updating and keys added

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -4,40 +4,8 @@ import {Button} from "semantic-ui-react";
 export default function InputForm(props) {
 
 const [emotions, setEmotions] = useState([])
-
-{/*
-  const fillItems = (item, i) => {
-    console.log(item)
-    let thisStatus = `status${item}`
-    let thisFunction = `set${item}`
-    return (
-      <div
-        key={`column-${i}-${item}`} className="computer column">
-      <div
-        key={`button-${i}-${item}`} className=
-        {variableStatus?"ui toggle button active":"ui toggle button"}
-        onClick={()=>{setButtonActive(thisStatus, ()=> {thisFunction})}}
-        >
-      <label
-        key={`label-${i}-${item}`}>{item}
-      </label>
-
-      </div>
-      </div>
-    )
-  }
-
-
-  const buildGroup = (group) => {
-    return group.map((item, i) => {
-      return(
-        fillItems(item,i)
-      )
-      }
-    )
-  }
-
-  */}
+const [thoughts, setThoughts] = useState([])
+const [behaviors, setBehaviors] = useState([])
 
   const getEmotions =()=>{
     const url = `${props.baseURL}/emotion/`
@@ -45,7 +13,7 @@ const [emotions, setEmotions] = useState([])
       method: 'GET'
       // , credentials: 'include'
     }
-    fetch(url)
+    fetch(url, requestOptions)
       .then(res => {
         return res.json();
       })
@@ -54,7 +22,6 @@ const [emotions, setEmotions] = useState([])
       });
   }
 
-
   const updateEmotion = async (id) =>{
     console.log(id+' was clicked')
     const url = `${props.baseURL}/emotion/${id}`
@@ -62,7 +29,7 @@ const [emotions, setEmotions] = useState([])
     console.log(emoObj)
     console.log(url)
 
-    const response = await fetch(url, {
+    await fetch(url, {
       method: "PUT",
       body: JSON.stringify({
         status: !emoObj["status"]
@@ -72,9 +39,7 @@ const [emotions, setEmotions] = useState([])
       },
       // credentials: "include"
     });
-
-
-
+    getEmotions()
   }
 
   const handleSubmit = () => {
@@ -95,9 +60,9 @@ const [emotions, setEmotions] = useState([])
     <div className="ui six column grid center aligned doubling">
       {emotions.map((emotion, e)=>{
         return (
-          <div className="computer column">
-            <div className="ui toggle button" onClick={()=>updateEmotion(emotion["id"])}>
-              <label>{emotion["emotion"]}</label>
+          <div key={`emotion-${emotion["id"]}-column`} className="computer column">
+            <div key={`emotion-${emotion["id"]}-button`} className={emotion["status"]?"ui toggle button active":"ui toggle button"} onClick={()=>updateEmotion(emotion["id"])}>
+              <label key={`emotion-${emotion["id"]}-label`}>{emotion["emotion"]}</label>
             </div>
           </div>
         )
@@ -115,12 +80,6 @@ const [emotions, setEmotions] = useState([])
 
 <br/>
 <br/>
-{emotions.map((emotionObject, e) => {
-          return (
-            <p key={e}>{emotionObject["emotion"]}: {emotionObject["status"]}</p>
-          )
-        })}
-
 
 
 </div>
