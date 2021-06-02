bab6a9e344a8979d34f2d5bcaddf5ce52a23e8d4
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-06-01 20:35:16 -0500

    Added card, like and complete buttons to suggested exercise component. Next to make like and submit buttons functional. Consider adding number of completions to card.

--- src/components/SuggestedExercise.js
+++ src/components/SuggestedExercise.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import {Button} from "semantic-ui-react";
 
 export default function SuggestedExercise(props) {
   const [exercise,setExercise]=useState({})
@@ -31,11 +32,48 @@ export default function SuggestedExercise(props) {
 
   return (
     <div id='exerciseContainer'>
-      <div class="ui segment">
-        <h2 id="exerciseHeader" class="ui center aligned header">{exercise["name"]}</h2>
-        <div class="ui clearing divider"></div>
+{/*
+      <div className="ui segment">
+        <h2 id="exerciseHeader" className="ui center aligned header">{exercise["name"]}</h2>
+
+
+        <div className="ui clearing divider"></div>
+
+
         <p id="exerciseDescription">{exercise["description"]}</p>
+
+
+        <div className="ui section divider"></div>
+
+        <div className='ui text container'>
+         <Button type='submit'><i className='check circle outline icon big'></i>Complete</Button>
+        </div>
       </div>
+*/}
+    <div class="ui fluid raised card">
+        <div class="content">
+          <div class="header">{exercise["name"]}</div>
+        </div>
+        <div class="content">
+          <h4 class="ui sub header">Exercise</h4>
+          <div class="ui large feed">
+              <div class="content">
+                <div class="description left aligned">
+                  <p>{exercise["description"]} Cute dogs come in a variety of shapes and sizes. Some cute dogs are cute for their adorable faces, others for their tiny stature, and even others for their massive size.</p>
+                  <p>Many people also have their own barometers for what makes a cute dog.</p>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="extra content">
+            <button class="ui button like"><i className='like icon big'/>Like</button>
+            <button class="ui button"><i className='check icon big'/>Complete</button>
+        </div>
+      </div>
+
+
+
     </div>
 
 
