ce64198b6070e31b7de77de32a9f9199102bed78
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-06-04 20:15:45 -0500

    added reset ETBs to use effect dependingi on getting suggested exercise

--- src/components/SuggestedExercise.js
+++ src/components/SuggestedExercise.js
@@ -129,7 +129,7 @@ try {
 
 
   useEffect(getSuggestedExercise, [])
-  // useEffect(resetETBs,[])
+  useEffect(resetETBs,[getSuggestedExercise]])
   // useEffect(addUserExercise,[])
   console.log('userexerciseID:'+userExerciseID)
 
